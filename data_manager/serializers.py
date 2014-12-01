c02fffb02423f7901d8a5503c6b0a59006f390b9
Eric Drechsel <eric@pointnineseven.com>
2014-12-01 15:08:22 -0700

    add name field to BriefLayerSerializer

--- data_manager/serializers.py
+++ data_manager/serializers.py
@@ -4,4 +4,4 @@ from .models import Layer
 class BriefLayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Layer
-        fields = ('id', 'layer_type', 'url')
+        fields = ('id', 'name', 'layer_type', 'url')
