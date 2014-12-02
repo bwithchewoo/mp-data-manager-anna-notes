9a464c6f3930678f2269249f24239642247c46ea
Eric Drechsel <eric@pointnineseven.com>
2014-12-02 16:42:13 -0700

    add serializer fields for vector styling and arcrest

--- data_manager/serializers.py
+++ data_manager/serializers.py
@@ -4,4 +4,4 @@ from .models import Layer
 class BriefLayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Layer
-        fields = ('id', 'name', 'layer_type', 'url')
+        fields = ('id', 'name', 'layer_type', 'url', 'opacity', 'vector_color', 'vector_fill', 'vector_outline_color', 'vector_outline_opacity', 'arcgis_layers')
