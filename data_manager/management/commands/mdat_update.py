15234d69102c9a63d4f1e2567b9e801366d8d9f1
Drew <drew@pointnineseven.com>
2016-05-24 14:30:17 -0700

    update mdat_update command

--- data_manager/management/commands/mdat_update.py
+++ data_manager/management/commands/mdat_update.py
@@ -15,63 +15,79 @@ class Command(BaseCommand):
 
         #grab all parent service directories for enpoint
         r = requests.get(mdat_rest_path+'MDAT?f=json')
-        parent_json = r.json()
-        mdat_dirs = parent_json['services']
+        #request status is OK
+        if r.status_code != 200:
+            return
+        else:
+            print "**** Request 200 - is OK *****"
+            parent_json = r.json()
+            mdat_dirs = parent_json['services']
 
-        #loop through mdat service *parent* directory array
-        for directory in mdat_dirs:
-            print "***** Entering %s *****" % directory['name']
-            #defaults for parent directories
-            parent_defaults = { 
-                'name':directory['name'], 
-                'layer_type':'checkbox',
-            }
-            #does layer exist?
-            try:
-                obj = Layer.objects.get(themes=mdat, name=directory['name'])
-            #create parent layer/directory - if not
-            except Layer.DoesNotExist:
-                print "***** Adding %s *****" % directory['name']
-                obj = Layer.objects.create(**parent_defaults)
-                obj.site = [1,2]
-                obj.themes = [mdat_id]
-                obj.save()
-
-            #get pk for current layer
-            layer_id = obj.pk
-
-            #set path
-            layer_path = mdat_rest_path+directory['name']+'/MapServer'
-
-            #grab all layers of parent endpoint in the loop
-            blob = requests.get(layer_path+'?f=json')
-            layer_json = blob.json()
-            mdat_layers = layer_json['layers']
-            layer_url = layer_path + '/export'
-
-            #loop through layers within parent directory array
-            for layer in mdat_layers:
-                print "***** Looping through %s *****" % layer['name']
-                layer_defaults = {
-                    'name':layer['name'],
-                    'layer_type':'ArcRest',
-                    'arcgis_layers':layer['id'],
-                    'is_sublayer': 1,
-                    'url':layer_url
+            #loop through mdat service *parent* directory array
+            for directory in mdat_dirs:
+                print "***** Entering %s *****" % directory['name']
+                #defaults for parent directories
+                parent_defaults = { 
+                    'name':directory['name'], 
+                    'layer_type':'checkbox',
                 }
 
-                try:
-                    lyr = Layer.objects.get(themes=mdat, name=layer['name'], url=layer_url)
-                #create layers of parent directory - if they don't exist
-                except Layer.DoesNotExist:
-                    print "***** Adding %s *****" % layer['name']
-                    lyr = Layer.objects.create(**layer_defaults)
-                    lyr.site = [1,2]
-                    lyr.themes = [mdat_id]
-                    lyr.sublayers = [layer_id]
-                    lyr.save()
-                    #sublayer fields need to be filled in for parent dir
-                    obj.sublayers = [layer_id]
-                    obj.save()
+                if directory['type'] != 'MapServer':
+                    print "***** %s is not a MapServer Layer" % directory['name']
+                    return
+                #continue on if it's a MapServer layer
+                else:
+                    #does layer exist?
+                    try:
+                        obj = Layer.objects.get(themes=mdat, name=directory['name'])
+                    #create parent layer/directory - if not
+                    except Layer.DoesNotExist:
+                        print "***** Adding %s *****" % directory['name']
+                        obj = Layer.objects.create(**parent_defaults)
+                        obj.site = [1,2]
+                        obj.themes = [mdat_id]
+                        obj.save()
+
+                    #get pk for current layer
+                    layer_id = obj.pk
+
+                    #set path
+                    layer_path = mdat_rest_path+directory['name']+'/MapServer'
+
+                    #grab all layers of parent endpoint in the loop
+                    blob = requests.get(layer_path+'?f=json')
+                    layer_json = blob.json()
+                    mdat_layers = layer_json['layers']
+                    layer_url = layer_path + '/export'
+
+                    #loop through layers within parent directory array
+                    for layer in mdat_layers:
+                        print "***** Looping through %s *****" % layer['name']
+                        layer_defaults = {
+                            'name':layer['name'],
+                            'layer_type':'ArcRest',
+                            'arcgis_layers':layer['id'],
+                            'is_sublayer': 1,
+                            'url':layer_url
+                        }
+
+                        try:
+                            lyr = Layer.objects.get(themes=mdat, arcgis_layers=layer['id'], url=layer_url)
+                            #update name, just incase it changed
+                            lyr.name = layer['name']
+                            lyr.save()
+                            print "***** Layer %s exits *****" % layer['name']
+                        #create layers of parent directory - if they don't exist
+                        except Layer.DoesNotExist:
+                            print "***** Adding %s *****" % layer['name']
+                            lyr = Layer.objects.create(**layer_defaults)
+                            lyr.site = [1,2]
+                            lyr.themes = [mdat_id]                              
+                            lyr.sublayers = [layer_id]
+                            lyr.save()
+
+                            #sublayer fields need to be filled with pks for parent dir
+                            obj.sublayers.add(lyr.pk)
+                            obj.save()
 
 
