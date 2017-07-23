7abafd7b8fff8b902093f6c5b0e132df77e2b82e
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-07-23 14:01:44 -0700

    adding migration file for new ESPIS fields

---
+++ data_manager/migrations/0011_auto_20170723_2036.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('data_manager', '0010_layer_disable_arcgis_attributes'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='layer',
+            name='espis_enabled',
+            field=models.BooleanField(default=False),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='layer',
+            name='espis_region',
+            field=models.CharField(blank=True, max_length=100, null=True, help_text=b'Region to search within', choices=[(b'Mid Atlantic', b'Mid Atlantic')]),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='layer',
+            name='espis_search',
+            field=models.CharField(help_text=b'keyphrase search for ESPIS Link', max_length=255, null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
