95b1eceef697d39129630b6699299f2e097035d7
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-11-17 10:49:22 -0800

    forgot migration file for date published layer update

---
+++ data_manager/migrations/0018_layer_data_publish_date.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('data_manager', '0017_auto_20171117_0041'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='layer',
+            name='data_publish_date',
+            field=models.DateField(default=None, null=True, verbose_name=b'Date created/published', blank=True),
+            preserve_default=True,
+        ),
+    ]
