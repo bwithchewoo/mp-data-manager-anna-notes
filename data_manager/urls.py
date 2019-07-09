52e456dc8918c3636ea4d9ee476012b901bb9a57
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-07-09 15:30:30 -0700

    django 2 url patterns update

--- data_manager/urls.py
+++ data_manager/urls.py
@@ -1,15 +1,17 @@
-from django.conf.urls import url, include, patterns
+# from django.conf.urls import url, include, patterns
+from django.urls import re_path, include
 from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
 router.register(r'layers', views.LayerViewSet)
 
-urlpatterns = patterns('',
-    url(r'^nested_admin/', include('nested_admin.urls')),
-    url(r'^api/', include(router.urls)),
-    (r'^layer/([A-Za-z0-9_-]+)$', views.update_layer),
-    (r'^layer$', views.create_layer),
-    (r'^get_json$', views.get_json),
-    (r'^wms_capabilities', views.wms_request_capabilities),
-)
+urlpatterns = [
+    #'',
+    re_path(r'^nested_admin/', include('nested_admin.urls')),
+    re_path(r'^api/', include(router.urls)),
+    re_path(r'^layer/([A-Za-z0-9_-]+)$', views.update_layer),
+    re_path(r'^layer$', views.create_layer),
+    re_path(r'^get_json$', views.get_json),
+    re_path(r'^wms_capabilities', views.wms_request_capabilities),
+]
