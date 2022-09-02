9bf7c291bac048e5033700721521fea83ac86f98
Ryan Hodges <ryan.d.hodges@gmail.com>
2022-09-02 14:58:10 -0700

    Present admin with any zoom limits in 'Style' section for ArcGIS layers

--- data_manager/static/data_manager/js/layer_form.js
+++ data_manager/static/data_manager/js/layer_form.js
@@ -507,7 +507,7 @@ assign_field_values_from_source_technology = function() {
       }
       if (url.toLowerCase().indexOf('/mapserver') >= 0 || url.toLowerCase().indexOf('/featureserver') >= 0) {
         $.ajax({
-          url: url + "?f=json",
+          url: url + "/layers?f=json",
           success: function(data) {
             if (typeof data != "object") {
               data = JSON.parse(data);
@@ -515,17 +515,46 @@ assign_field_values_from_source_technology = function() {
             layers = [];
             for (var i = 0; i < data.layers.length; i++) {
               var layer = data.layers[i];
-              layers.push({id:layer.id.toString(), name: layer.name});
+              if (layer.minScale) {
+                var minZoom = (Math.log(591657550.500000 /(layer.minScale/2))/Math.log(2)).toPrecision(3);
+              } else {
+                var minZoom = undefined;
+              }
+              if (layer.maxScale) {
+                var maxZoom = (Math.log(591657550.500000 /(layer.maxScale/2))/Math.log(2)).toPrecision(3);
+              } else {
+                var maxZoom = undefined;
+              }
+              layers.push({
+                id:layer.id.toString(), 
+                name: layer.name,
+                minZoom: minZoom,
+                maxZoom: maxZoom,
+                minResolution: layer.minScale,
+                maxResolution: layer.maxScale
+              });
             }
-            var table_element = "<div class='argis-details-table-wrapper'><table class='arcgis-details-layer-table'><tr><th>ID</th><th>Name</th><th>Link</th></tr>";
+            var layer_table_element = "<div class='argis-details-table-wrapper'><table class='arcgis-details-layer-table'><tr><th>ID</th><th>Name</th><th>Link</th></tr>";
             for (var i = 0; i < layers.length; i++) {
               layer = layers[i];
               var row = "<tr><td>" + layer.id + "</td><td>" + layer.name + "</td><td><a href='" + url + "/" + layer.id + "' target='_blank'>Details</a></td></tr>";
-              table_element = table_element + row;
+              layer_table_element = layer_table_element + row;
             }
-            table_element = table_element + "</table></div>";
+            layer_table_element = layer_table_element + "</table></div>";
             $('.arcgis-details-layer-table').remove();
-            $('div.field-box.field-arcgis_layers').append(table_element);
+            $('div.field-box.field-arcgis_layers').append(layer_table_element);
+
+            var zoom_table_element = "<div class='argis-zoom-table-wrapper'><table class='arcgis-zoom-layer-table'><tr><th>ID</th><th>Name</th><th>Min Zoom</th><th>Max Zoom</th></tr>";
+            for (var i = 0; i < layers.length; i++) {
+              layer = layers[i];
+              var row = "<tr><td>" + layer.id + "</td><td>" + layer.name + "</td><td>" + layer.minZoom + "</td><td>" + layer.maxZoom + "</td></tr>";
+              zoom_table_element = zoom_table_element + row;
+            }
+            zoom_table_element = zoom_table_element + "</table></div>";
+            $('.arcgis-zoom-layer-table').remove();
+            $('div.field-box.field-minZoom').append(zoom_table_element);
+
+
           }
         })
       }
