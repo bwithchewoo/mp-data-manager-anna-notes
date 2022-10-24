dd07856e8db4fb6b23a96dc460ef4ecd478f7bf2
Ryan Hodges <ryan.d.hodges@gmail.com>
2022-10-24 14:42:18 -0700

    credit where credit's due

--- data_manager/templatetags/migration_extras.py
+++ data_manager/templatetags/migration_extras.py
@@ -1,5 +1,8 @@
 from django.template.defaulttags import register
 
+# Borrowed directly from culebrón: 
+# https://stackoverflow.com/a/8000091/706797
+
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
